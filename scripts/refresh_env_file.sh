91fec9c663cc7a29399a4aff763169776df388fe
vinz3872 <vin.jacquier@gmail.com>
2020-12-05 20:35:06 +0100

    parse params, add usage, add conf

--- scripts/refresh_env_file.sh
+++ scripts/refresh_env_file.sh
@@ -1,45 +1,32 @@
 #!/bin/sh
 
-while read -r line; do
-  ([[ $line =~ ^[#|\[].*$ ]] || [[ -z $line ]]) && continue
-  arr=($line)
-  [[ "${#arr[@]}" != 2 ]] && echo "Error: Bad line format [$line]" >&2 && exit 1
-  export ${arr[0]^^}=${arr[1]}
+env_file_path=config/load_env.sh
+docker_env_file_path=config/env_file
+bin_folder=$PWD/bin
+
+echo "# This file is auto generated, do not modify it." > $env_file_path
+echo "# This file is auto generated, do not modify it." > $docker_env_file_path
+
+# Add project env variables
+echo "export LI_DOCKER_PATH_BINS=$bin_folder" >> $env_file_path
+echo "LI_DOCKER_PATH_BINS=$bin_folder" >> $docker_env_file_path
+echo "export LI_DOCKER_ENV_FILE_PATH=$docker_env_file_path" >> $env_file_path
+
+# Import env variables from config/.config
+while read -r env_name value; do
+  ([[ $env_name == '#' ]] || [[ -z $env_name ]] || [[ -z $value ]]) && continue
+  echo "export ${env_name^^}=${value}" >> $env_file_path
+  echo ${env_name^^}=${value} >> $docker_env_file_path
 done < config/.config
 
-li_project_path ~/.lang-instal
-li_bin_path ~/.lang-instal/bin
-echo $PROJECT_PATH
-$BIN_PATH
-
-
-
-main_path=$HOME/docker/path
-gem_path=$HOME/.gems
-docker_env_file_path=$PROJECT_PATH/config/env_file
-mkdir -p $main_path
-
-declare -a dirs=$(find $main_path -name "global")
+# Add installed languages binaries in path
+declare -a dirs=$(find $bin_folder -name "global")
 current_path=$PATH
 for i in $dirs; do
   # ${i%%[[:cntrl:]]}: remove \r (last elem)
   str=${i%%[[:cntrl:]]}
   current_path+=":$str"
 done
-current_path+=":$gem_path/bin"
 
-env_file='load_env.sh'
-echo "export GEM_HOME=$gem_path" > $env_file
-echo "GEM_HOME=$gem_path" > $docker_env_file_path
-
-echo "export GEM_PATH=$gem_path" >> $env_file
-echo "GEM_PATH=$gem_path" >> $docker_env_file_path
-
-echo "export BUNDLE_PATH=$gem_path" >> $env_file
-echo "BUNDLE_PATH=$gem_path" >> $docker_env_file_path
-
-echo "export DOCKER_PATH_BINS=$main_path" >> $env_file
-echo "DOCKER_PATH_BINS=$main_path" >> $docker_env_file_path
-
-echo "export PATH=$current_path" >> $env_file
-echo "export DOCKER_ENV_FILE_PATH=$docker_env_file_path" >> $env_file
+echo "export PATH=$current_path" >> $env_file_path
+# TODO: check if gem_path is needed in PATH variable
