9e8117631dd8b43bc4880b2aeab33b0d7310937d
vinz3872 <vin.jacquier@gmail.com>
2020-12-06 11:01:04 +0100

    add generic build and dockerfile normal / alpine

--- languages/default/build_alpine.sh
+++ languages/default/build_alpine.sh
@@ -1,39 +1,64 @@
-if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-  echo "Missing params, can't build"
-  exit
-fi
-bin_name=$1
-version=$2
-path=$3
-echo "Build $bin_name version $version"
+#!/bin/sh
 
-docker build -t "path_$bin_name:$version" \
+if [[ $# < 6 ]]; then
+  echo "Missing params, can't build"
+  exit 1
+fi
+
+language=$1
+version=$2
+binary_name=$3
+dockerfile_path=$4
+debug=$5
+verbose=$6
+
+docker build -t "li_$language:$version" \
   --build-arg USER_UID=`id -u` \
   --build-arg USER_GID=`id -g` \
   --build-arg USER_NAME=`id -un` \
   --build-arg VERSION=$version \
-  --build-arg BIN_NAME=$bin_name \
-  -f Dockerfile \
+  --build-arg LANGUAGE=$language \
+  -f $dockerfile_path \
   .
 
-if [ ! -d "$path/$bin_name/$version" ]; then
-  # init folder and copy files in parent folder
-  mkdir -p $path/$bin_name/$version
-  content_bin_sh="#!/bin/sh"
-  content_docker_generic='docker run -it --rm --network=host -v "/tmp:/tmp" -v "$HOME:$HOME" -v "$PWD:$PWD" -w $PWD -u `id -u` --env-file $DOCKER_ENV_FILE_PATH'
-  dirname=$(docker run -it --rm --network=host -v "$PWD:$PWD" -w $PWD -u `id -u` path_$bin_name:$version bash -c "which $bin_name | xargs dirname")
+dirname=$(docker run -it --rm --network=host -v "$PWD:$PWD" -w $PWD -u `id -u` li_$language:$version sh -c "which $binary_name | xargs dirname")
+dirname=${dirname%%[[:cntrl:]]}
 
-  declare -a bins=$(docker run -it --rm --network=host -v "$PWD:$PWD" -w $PWD -u `id -u` path_$bin_name:$version find ${dirname%%[[:cntrl:]]} -name "*.sh" -exec basename {} \;)
+# debug
+if [[ $debug == true ]]; then
+  content_bin_sh="#!/bin/sh"
+  content_docker_generic='docker run -it --rm --network=host -v "$PWD:$PWD" -w $PWD -u `id -u`'
+
+  declare -a bins=$(docker run -it --rm --network=host -v "$PWD:$PWD" -w $PWD -u `id -u` li_$language:$version find $dirname ! -name "*.sh" -exec basename {} \;)
   for i in $bins; do
     # ${i%%[[:cntrl:]]}: remove \r (last elem)
-    file_content="$content_bin_sh\n$content_docker_generic path_$bin_name:$version ${i%%[[:cntrl:]]}"
+    file_content="$content_bin_sh\n$content_docker_generic li_$language:$version ${i%%[[:cntrl:]]}"
     file_content+=' "$@"'
-    echo -e $file_content > $path/$bin_name/$version/${i%%[[:cntrl:]]}
-    chmod +x $path/$bin_name/$version/${i%%[[:cntrl:]]}
+    echo -e $file_content
   done
-fi
+else
+  bin_folder_path=$LI_DOCKER_PATH_BINS/$language/$version
+  global_bin_folder_path=$LI_DOCKER_PATH_BINS/$language/global
+  if [ ! -d $bin_folder_path ]; then
+    # init folder and copy files in parent folder
+    mkdir -p $bin_folder_path
+    content_bin_sh="#!/bin/sh"
+    content_docker_generic='docker run -it --rm --network=host -v "/tmp:/tmp" -v "$HOME:$HOME" -v "$PWD:$PWD" -w $PWD -u `id -u` --env-file $LI_DOCKER_ENV_FILE_PATH'
 
-if [ -d "$path/$bin_name/global" ]; then
-  rm -rf "$path/$bin_name/global"
+    declare -a bins=$(docker run -it --rm --network=host -v "$PWD:$PWD" -w $PWD -u `id -u` li_$language:$version find $dirname ! -name "*.sh" -exec basename {} \;)
+    for i in $bins; do
+      # ${i%%[[:cntrl:]]}: remove \r (last elem)
+      i=${i%%[[:cntrl:]]}
+      [[ $verbose == true ]] && echo "Binary found: $i"
+      file_content="$content_bin_sh\n$content_docker_generic li_$language:$version $i"
+      file_content+=' "$@"'
+      echo -e $file_content > $bin_folder_path/$i
+      chmod +x $bin_folder_path/$i
+    done
+  fi
+
+  if [ -d $global_bin_folder_path ]; then
+    rm -rf $global_bin_folder_path
+  fi
+  cp -R $bin_folder_path $global_bin_folder_path
 fi
-cp -R $path/$bin_name/$version $path/$bin_name/global
